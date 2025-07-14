d42a8a315e4032448ed030ca98d4f56a317af89c
Ernie Francis <devin_98@msn.com>
2025-07-14 00:58:27 -0400

    initial Push

---
+++ src/app/layout.tsx
@@ -0,0 +1,41 @@
+import * as React from "react";
+import { NextAppProvider } from "@toolpad/core/nextjs";
+import { AppRouterCacheProvider } from "@mui/material-nextjs/v15-appRouter";
+import DashboardIcon from "@mui/icons-material/Dashboard";
+import PersonIcon from "@mui/icons-material/Person";
+import type { Navigation } from "@toolpad/core/AppProvider";
+
+const NAVIGATION: Navigation = [
+  {
+    kind: "header",
+    title: "Main items",
+  },
+  {
+    segment: "projects",
+    title: "Projects",
+    icon: <DashboardIcon />,
+  },
+  {
+    segment: "resume",
+    title: "Resume",
+    icon: <PersonIcon />,
+  },
+];
+
+const BRANDING = {
+  title: "Ernie Francis",
+};
+
+export default async function RootLayout(props: { children: React.ReactNode }) {
+  return (
+    <html lang="en" data-toolpad-color-scheme="light">
+      <body>
+        <AppRouterCacheProvider options={{ enableCssLayer: true }}>
+          <NextAppProvider navigation={NAVIGATION} branding={BRANDING}>
+            {props.children}
+          </NextAppProvider>
+        </AppRouterCacheProvider>
+      </body>
+    </html>
+  );
+}
